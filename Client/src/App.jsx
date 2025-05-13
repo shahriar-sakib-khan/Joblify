af6e2e9bf693d6ae9daac390e0f4fdf8453b3719
Shahriar Sakib <shahriarsakib.khan@gmail.com>
2025-05-13 11:01:32 +0600

    complete edit job page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -13,16 +13,19 @@ import {
   AllJobs,
   Profile,
   Admin,
+  EditJob,
 } from "./pages";
 
 // <============================> Actions <============================>
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
 import { action as addJobsAction } from "./pages/AddJob";
+import { action as editJobAction } from "./pages/EditJob";
 
 // <============================> Loaders <============================>
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
+import { loader as editJobLoader } from "./pages/EditJob";
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -75,6 +78,12 @@ const router = createBrowserRouter([
           {
             path: "admin",
             element: <Admin />,
+          },
+          {
+            path: "edit-job/:id",
+            element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
           },
         ],
       },
