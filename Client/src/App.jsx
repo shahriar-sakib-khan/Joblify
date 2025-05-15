93512afc60adf46ab46f9c757d167aaa71dbb8ee
Shahriar Sakib <shahriarsakib.khan@gmail.com>
2025-05-15 12:05:41 +0600

    complete admin page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -27,6 +27,7 @@ import { action as deleteJobAction } from "./pages/DeleteJob";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
+import { loader as adminLoader } from "./pages/Admin";
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -79,6 +80,7 @@ const router = createBrowserRouter([
           {
             path: "admin",
             element: <Admin />,
+            loader: adminLoader,
           },
           {
             path: "edit-job/:id",
